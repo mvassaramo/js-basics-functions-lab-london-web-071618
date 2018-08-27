51621009fe6279319b260ced7bffc149495678a9
github username <github email address>
2018-08-27 10:18:31 +0100

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function calculatesFarePrice(start, end) {
       return 0;
     }
     else if (distance > 400 && distance <= 2000) {
-      return ((distance - 400) * 0.02);
+      return (distance - 400) * 0.02;
     }
     else if (distance > 2000 && distance <= 2500) {
       return 25;
